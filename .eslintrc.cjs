5fb90452e0eecc95bc8ad36fb923d0b551c188ba
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-05-14 19:25:37 +0400

    feat: configure i18n and eslint
    - added localizations for landing view
    - configured strict type checking for ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,15 @@ module.exports = {
   root: true,
   extends: [
     'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended',
     'eslint:recommended',
     '@vue/eslint-config-prettier'
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
   },
   rules: {
     // override/add rules settings here, such as:
