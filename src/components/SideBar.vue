9639bb26fb33ad92ec848f5bf9abe35b37844c42
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-06-08 16:34:49 +0400

    feat: implement navigation dropdown for mobile screen
    - useSearchBar.ts holds logic that is absolutely common between different searchbars
    - SideBar.vue was modified to support mobile screen size, and it is now used inside a navigation dropdown
    - SharedHeader.vue now supports different views to conditionally display NavigationDropdown.vue and notifications

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -8,22 +8,24 @@ const store = useUserAuthStore();
 
 <template>
   <div class="flex flex-col gap-10 whitespace-nowrap fill-white">
-    <div class="flex items-center gap-6">
+    <div class="flex items-center gap-5 laptop:gap-6">
       <img
         :src="store.user?.image"
         alt="profile image"
-        class="h-15 w-15 rounded-full border-2 object-cover object-center"
+        class="h-10 w-10 rounded-full border-2 object-cover object-center laptop:h-15 laptop:w-15"
         :class="
           $route.name === 'Profile' ? 'border-scarlet' : 'border-transparent'
         "
       />
-      <div class="flex flex-col gap-2.5">
-        <p class="max-w-36 overflow-visible text-2xl capitalize leading-5">
+      <div class="flex flex-col gap-1.5 laptop:gap-0">
+        <p
+          class="max-w-36 overflow-visible text-xl capitalize leading-5 laptop:text-2xl"
+        >
           {{ store.user?.name }}
         </p>
         <RouterLink
           :to="{ name: 'Profile' }"
-          class="inline-block text-base leading-4 text-gray-400"
+          class="inline-block text-sm leading-4 text-gray-400 laptop:text-base"
         >
           {{ $t("sidebar.edit_your_profile") }}
         </RouterLink>
@@ -31,18 +33,18 @@ const store = useUserAuthStore();
     </div>
     <RouterLink
       :to="{ name: 'Quotes' }"
-      class="flex items-center gap-10 pl-2.5 text-2xl"
+      class="flex items-center gap-7 pl-2 text-xl laptop:gap-10 laptop:pl-2.5 laptop:text-2xl"
       active-class="fill-red-500"
     >
-      <IconsHouse class="fill-inherit" />
+      <IconsHouse class="h-6 w-6 fill-inherit laptop:h-auto laptop:w-auto" />
       {{ $t("sidebar.news_feed") }}
     </RouterLink>
     <RouterLink
       :to="{ name: 'Movies' }"
-      class="flex items-center gap-10 pl-2.5 text-2xl"
+      class="flex items-center gap-7 pl-2.5 text-xl laptop:gap-10 laptop:text-2xl"
       active-class="fill-red-500"
     >
-      <IconsCamera class="fill-inherit" />
+      <IconsCamera class="h-6 w-6 fill-inherit laptop:h-8 laptop:w-8" />
       {{ $t("sidebar.list_of_movies") }}
     </RouterLink>
   </div>
---
+++ src/composables/useSearchBar.ts
@@ -0,0 +1,40 @@
+import { ref, watch } from "vue";
+import { useRouter } from "vue-router";
+
+export function useSearchBar() {
+  const active = ref(false);
+  const inputRef = ref<HTMLInputElement | null>(null);
+  const inputText = ref("");
+  const router = useRouter();
+  const timer = ref<NodeJS.Timeout | null>(null);
+
+  watch(inputText, () => {
+    const input = inputText.value.trim();
+    if (timer.value) {
+      clearTimeout(Number(timer.value));
+    }
+    timer.value = setTimeout(() => {
+      if (input.startsWith("@")) {
+        router.push({ query: { "filter[title]": input.slice(1) } });
+      } else if (input.startsWith("#")) {
+        router.push({ query: { "filter[quote]": input.slice(1) } });
+      } else {
+        router.push({ query: { "filter[title]": input.slice(1) } });
+      }
+    }, 550);
+  });
+
+  const focusInput = () => {
+    console.log(inputRef.value);
+    if (inputRef.value) {
+      (inputRef.value as HTMLInputElement).focus();
+    }
+  };
+
+  return {
+    active,
+    inputRef,
+    inputText,
+    focusInput,
+  };
+}
