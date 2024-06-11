20043ad8ce51e05253a6dc99500c4e3de81683a6
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-06-11 05:31:41 +0400

    chore: import styles and locales
    - two files were misplaced previously

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     extend: {
       fontSize: {
         "3xl-plus": ["2rem", "2.5rem"],
+        "xs-minus": ["0.5rem", "0.75rem"],
       },
       scale: {
         65: "0.65",
@@ -16,6 +17,7 @@ export default {
         13: "3.25rem",
         15: "3.75rem",
         47: "11.75rem",
+        84: "21rem",
       },
       padding: {
         30: "7.5rem",
@@ -81,7 +83,19 @@ export default {
         crimson: "#F3426C",
         gainsboro: "#D9D9D9",
       },
+      backgroundSize: {
+        300: "300% 300%",
+        250: "250% 250%",
+        200: "200% 200%",
+        150: "150% 150%",
+      },
       backgroundImage: {
+        "landing-view-section-overlay":
+          "linear-gradient(90deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 100%)",
+        "landing-view-section-overlay-laptop":
+          "linear-gradient( 90deg, #000000 0%, rgba(0, 0, 0, 0) 55.21%, rgba(0, 0, 0, 0) 100%)",
+        "landing-view-gradient":
+          "linear-gradient(180deg, #11101A 0%, #08080D 50.52%, rgba(0, 0, 0, 0) 100%)",
         "gradient-radial":
           "linear-gradient(187.16deg, #181623 0.07%, #191725 51.65%, #0D0B14 98.75%)",
         "movie-interstellar": 'url("@/assets/images/movie_interstellar.png")',
@@ -92,6 +106,8 @@ export default {
         "action-modal-bg":
           "linear-gradient(112.94deg, rgba(239, 239, 239, 0.3) -1.81%, rgba(239, 239, 239, 0.00514528) 102.5%, rgba(1, 1, 1, 0.00260417) 102.51%, rgba(239, 239, 239, 0.05) 102.52%)",
         "gradient-none": "none",
+        "movie-royal-tenenbaums-with-gradient":
+          'linear-gradient(180deg, rgba(17, 16, 26, 0.95) 0%, rgba(8, 8, 13, 0.13) 50%, rgba(0, 0, 0, 0) 100%), url("@/assets/images/movie_royal_tenenbaums.png")',
       },
       boxShadow: {
         overlay: "inset 0 0 200px 0 rgba(0, 0, 0, 0.75)",
